d39f6df7bef540d3449b8860bf092b32ad472e87
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-07-06 14:24:35 +0530

    Update botHandeling.py

--- plugins/components/botHandeling.py
+++ plugins/components/botHandeling.py
@@ -55,9 +55,9 @@ async def set_stream_link(client, message):
             await db.set_stream_link(link)
             await message.reply("Successfully set stream link!")
         else:
-            await message.reply("Usage: /stream https://t.me/Ott_Sandhu_Owner_bot or http://t.me/Ott_Sandhu_Owner_bot")
+            await message.reply("Usage: /stream https://t.me/IpapcornProbot http://t.me/Ipopcornprobot")
     except IndexError:
-        await message.reply("Usage: /stream https://t.me/Ott_Sandhu_Owner_bot or http://t.me/Ott_Sandhu_Owner_bot")
+        await message.reply("Usage: /stream https://t.me/IpapcornProbot or https://t.me/IpapcornProbot")
         return
 
 
@@ -88,17 +88,17 @@ def checkIfLinkIsValid(link):
 @Client.on_message(filters.command("m_grp") & filters.user(ADMINS))
 async def m_grp(client, message):
     links = []
-    link = await client.ask(message.chat.id ,"send me your pm search grp link or send /skiplink to skip , default is Ott_Sandhu")
+    link = await client.ask(message.chat.id ,"send me your pm search grp link or send /skiplink to skip , default is UniformMovies")
     if link.text == "/skiplink":
-        links.append("https://t.me/Ott_Sandhu")
+        links.append("https://t.me/UniformMovies")
     else:
         if checkIfLinkIsValid(link.text):
             links.append(link.text)
         else:
             await message.reply("Invalid link")
-    link1 = await client.ask(message.chat.id ,"send me your movies grp link or send /skiplink to skip . default is Ott_Sandhu")
+    link1 = await client.ask(message.chat.id ,"send me your movies grp link or send /skiplink to skip . default is UniformMovies")
     if link1.text == "/skiplink":
-        links.append("https://t.me/Ott_Sandhu")
+        links.append("https://t.me/UniformMovies")
     else:
         if checkIfLinkIsValid(link1.text):
             links.append(link1.text)
